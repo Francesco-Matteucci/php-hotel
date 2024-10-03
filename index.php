0bd22da68e6188ec286b2c42bdab39654072d6c1
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-03 13:46:11 +0200

    Refactor table formatting with Bootstrap

--- index.php
+++ index.php
@@ -47,18 +47,40 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista Hotel</title>
+    <!-- Bootstrap style -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Lista Hotels</h1>
 
-    <?php foreach ($hotels as $hotel) { ?>
-    <h3>- <?= $hotel['name'] ?> -</h3>
-    <p>Descrizione:<?= $hotel['description'] ?></p>
-    <p>Parcheggio:<?= $hotel['parking'] ? 'Si' : 'No' ?></p>
-    <p>Voto:<?= $hotel['vote'] ?> stelle</p>
-    <p>Distanza dal centro:<?= $hotel['distance_to_center'] ?> km</p>
-    <?php } ?>
+    <div class="container">
+        <h1 class="my-4 text-center">Lista Hotels</h1>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Parcheggio</th>
+                    <th>Voto</th>
+                    <th>Distanza dal centro (km)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td class="fw-bold"><?= $hotel['name'] ?></td>
+                    <td><?= $hotel['description'] ?></td>
+                    <td><?= $hotel['parking'] ? 'Si' : 'No' ?></td>
+                    <td><?= $hotel['vote'] ?> stelle</td>
+                    <td><?= $hotel['distance_to_center'] ?> km</td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
 </body>
 
 </html>
